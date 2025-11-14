5d0044845a0608ee33c4a8c06877f8e49421c3f1
rodrigoess <rodrigoessilva2008@gmail.com>
2025-11-14 18:01:08 +0000

    segundo commit

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // No additional JS needed for this simple layout
+
+// Add click event to experience and project items
+document.querySelectorAll(".experience-item, .project-item").forEach((item) => {
+  item.addEventListener("click", () => {
+    const url = item.getAttribute("data-url");
+    if (url && url !== "#") {
+      window.open(url, "_blank");
+    }
+  });
+});
